90119815b90d5ebce6122727671b70db90765bf0
Vu Huynh <huynhnguyenvu89@gmail.com>
2018-09-06 07:22:56 -0700

    Changed style

--- app/src/main/java/com/example/vuhuynh/vitacare_android/MainActivity.kt
+++ app/src/main/java/com/example/vuhuynh/vitacare_android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vuhuynh.vitacare_android
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,7 +29,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+
 }
---
+++ app/src/main/java/com/example/vuhuynh/vitacare_android/VitaCareApp.kt
@@ -0,0 +1,13 @@
+package com.example.vuhuynh.vitacare_android
+
+import android.app.Application
+import android.content.Context
+import android.view.View
+import android.view.Window
+
+class VitaCareApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
